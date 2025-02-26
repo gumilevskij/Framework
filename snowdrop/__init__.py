28c8f1bfdb6172adcc92c7afccd03a0570dd870b
gumilevskij <agoumilevski@gmail.com>
2025-02-26 15:01:10 -0500

    Update __init__.py

--- snowdrop/__init__.py
+++ snowdrop/__init__.py
@@ -1,6 +1,6 @@
 import os,sys
 path = os.path.dirname(os.path.abspath(__file__))
-working_dir = os.path.abspath(path + "/../../..")
+working_dir = os.path.abspath(path + "/..")
 sys.path.append(working_dir)
 os.chdir(working_dir)
 
