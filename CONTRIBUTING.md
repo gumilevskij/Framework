3f3fc97b96bdbeb3aaa624d929776952fce7e9da
Alexei Goumilevski <agoumilevski@gmail.com>
2025-07-01 00:29:05 -0400

    Add files via upload
    Refactor projcet

---
+++ CONTRIBUTING.md
@@ -0,0 +1,20 @@
+# Contributing
+*Snowdrop* is an open source project and welcomes contributions! Here are some ways to help:
+
+* Writing and improving the documentation
+* Reporting or fixing bugs
+* Contributing features or enhancements
+* Writing model YAML model files and applying *Snowdrop* to solve those models
+* Creating demonstration projects or tutorials
+
+
+# Reporting Bugs
+Bugs and issues can be reported in the issue tracker using the [bug report form](https://github.com/gumilevskij/framework/issues/new). Please check the bug has not already been reported by [searching the issue tracker](https://github.com/gumilevskij/framework/issues) before submitting a new issue!
+
+
+# Feature Requests
+Feature requests can be made in the issue tracker using the [feature report form](https://github.com/gumilevskij/framework/issues/new). 
+
+
+# Solving Issues
+To submit bug fixes, open a new GitHub pull request with the bug fix. The PR description should clearly describe the problem and solution and should include the relevant issue numbers if applicable. Instructions for how to set up a local *Snowdrop* development environment and run the tests can be found in the [developers documentation](https://github.com/gumilevskij/Framework/tree/main/snowdrop/docs/UserGuide.pdf).
---
+++ tests/test_models_import.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 23 14:14:16 2019
+@author: AGoumilevski
+
+Simulates a set of basic shocks.  
+"""
+
+import os
+import sys
+import pandas as pd
+import numpy as np
+
+path = os.path.dirname(os.path.abspath(__file__))
+working_dir = os.path.abspath(path+"/..")
+sys.path.append(working_dir)
+os.chdir(working_dir)
+
+from snowdrop.src.driver import run
+from snowdrop.src.model.factory import import_model
+from snowdrop.src.graphs.util import plotTimeSeries
+
+
+def model_import(file_path, list_variables, list_shocks, shocks, list_titles, irf = True,calibration={}, freq=None, rng=None, sizes = [1,1]):
+    """Test import of model files. """
+    
+    # Create model
+    model = import_model(file_path,return_interface=False,calibration=calibration)
+    
+    path_to_dir = os.path.abspath(os.path.join(working_dir,"graphs"))
+    
+    if not freq is None:
+        model.options["frequency"] = freq
+    if not rng is None:
+        model.options["range"] = rng
+    model.options["periods"] = [[2025,7,1]]
+    #model.options["periods"] = [1]
+    var_names = model.symbols["variables"]
+    shock_names = model.symbols["shocks"]
+    n_shocks = len(shock_names)
+    num_shocks = len(list_shocks)
+    
+    shock_values = np.zeros(n_shocks)
+    for i in range(num_shocks):
+        # Set shocks
+        shock_name = list_shocks[i]
+        if shock_name in shock_names:
+            ind = [i for i,x in enumerate(shock_names) if x==shock_name][0]
+            shock_values[ind] = shocks[i]
+        model.options["shock_values"] = shock_values
+        
+        # Find solution
+        results,rng_date = run(model=model,irf=irf)
+        rows,columns = results.shape
+        
+        d = {}
+        for j in range(columns):
+            n = var_names[j]
+            data = results[:,j] 
+            ts = pd.Series(data[1:-1], rng_date)
+            d[n] = ts
+    
+        header = list_titles[i]
+        if len(list_variables) == 1:
+            titles = ['Nominal Interest Rate']
+        else:
+            titles = list_variables
+        series = [[d[k]] for k in list_variables]
+        labels = None
+        plotTimeSeries(path_to_dir=path_to_dir,header=header,titles=titles,labels=labels,series=series,sizes=sizes)
+    
+
+def test(rng= ["2025-1-1","2035-1-1"],freq=1):
+    """
+    Main program.
+    """
+    # 1. Yaml model file
+    # Unit shock to nominal interest rate
+    file_path = os.path.abspath(os.path.join(working_dir,'supplements/models/MPAF/model.yaml'))
+    model_import(file_path=file_path,list_variables=['RS'],list_shocks=['SHK_RS'],shocks=[1],list_titles=['Policy Interest Rate'],freq=freq,rng=rng)
+       
+    # 2. Dynare model file
+    # Unit shock to nominal interest rate
+    file_path = os.path.abspath(os.path.join(working_dir,'supplements/models/MPAF/model.mod'))
+    model_import(file_path=file_path,list_variables=['RS'],list_shocks=['SHK_RS'],shocks=[1],list_titles=['Policy Interest Rate'],freq=freq,rng=rng)
+     
+    # 3. Iris model file
+    # Shock to total factor productivity
+    calib_params = { "beta": 0.99, "delta": 0.03, "gamma":0.5, "rho":0.8, "Y":1.25, "C":0.78, "r":0.01, "K":15.55, "a":0.1, "A":0.1}
+    file_path = os.path.abspath(os.path.join(working_dir,'supplements/models/TOY/RBC.model'))
+    model_import(file_path=file_path,list_variables=['Y','C','K','r','A'],list_shocks=['ea'],shocks=[0.1],list_titles=['Macroeconomic Variables'],
+                 irf=False,calibration=calib_params,freq=freq,rng=rng,sizes=[3,2])
+
+
+if __name__ == '__main__':
+    """
+    Main program.
+    """
+    rng =  ["2025-1-1","2035-1-1"]; freq = 1 # Quarterly
+    test(rng,freq)
